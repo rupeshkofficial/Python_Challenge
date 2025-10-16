13c95003dc984e5a2b026befa7c6d9f603dba7b6
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-16 11:47:25 +0530

    Day 1

---
+++ Basic/comments.py
@@ -0,0 +1,34 @@
+# We are learning comments
+print("This is Rupesh")  # This is print statment
+print("We are learning comments") # This is print statments
+
+# Single line comments
+
+# print("R")
+# print("S")
+# print("J")
+
+# Multi line comments ---> use triple single quotes / use triple double quotes
+
+# Comments in tripe single quotes
+'''
+jai ho
+jai ho
+jai ho
+'''
+
+# comments in triple double quotes
+
+"""
+Jai ho
+jai ho
+jai ho
+
+
+
+"""
+
+
+
+
+

9ede17dfc5001e3ff839fc95f16f98169addd5d9
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-16 15:17:50 +0530

    Day_1

---
+++ Basic/que_4.py
@@ -0,0 +1,10 @@
+# To calculate the average of three number enter by user
+
+num1 = int(input("Enter First Number:"))
+num2 = int(input("Enter First Number:"))
+num3 = int(input("Enter First Number:"))
+
+num4 = num1 + num2 + num3 
+average = num4 / 3
+
+print(f"The Average of three number entered by user is: {average}")
---
+++ Basic/que_5.py
@@ -0,0 +1,7 @@
+# To convert float to integer and vice versa
+
+x = float(input("Enter any float number:"))
+y = int(x)
+z = float(y)
+print(f"The entered float number is {x} and their integer conversion is: {y} and again the float conversion of {y} is {z}")
+

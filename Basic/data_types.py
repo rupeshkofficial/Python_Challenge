e01c3f2384116d73a5687cff0f357fe54029553f
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-16 12:58:18 +0530

    Day_1

---
+++ Basic/data_types.py
@@ -0,0 +1,11 @@
+# Python Data Types
+
+# 1. Numeric --> Integers, floats and complex
+
+# 2. Dictionary {} --> key value pair.  Examples--> marks = {"Rupesh": 88, "Raj": 55}
+
+# 3. Sequence Types --> List [], Tuple () and Strings
+
+# 4. Set 
+
+# 5. Boolean ---> TRUE and FALSE

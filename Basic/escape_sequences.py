da7d67bbbc08f9f0a93cff8c9f659f8d5237e25c
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-16 12:16:25 +0530

    Day_1

---
+++ Basic/escape_sequences.py
@@ -0,0 +1,43 @@
+# Escape Sequences / Escape Charatcers ---> Illegal character of python that never printed as part of the output.
+
+"""
+
+\' --> Single Quotation
+\" --> Double Quotation
+\\ --> Backslash
+\n --> New Line
+\t --> tab
+\b --> Backspace
+
+
+"""
+
+# \n --> New Line
+print("My name is Rupesh\nI am from Bihar.\nWant to enjoy life.")
+
+# \t --> tab
+print("Rupesh\t\t\t\t\t\t\tKumar")
+
+
+# This is "code and debug"
+print("This is code and debug.")
+print("This is 'code and debug'.")
+print('This is "code and debug".')
+
+# \' --> Single Quotation and \" --> Double Quotation
+print("This is \"code and debug\" ")
+print ("This is \'code and debug\' ")
+
+
+# \\ --> Backslash
+#Want to print like this --> My name is R\nupesh
+
+print("My name is R\\nupesh")
+
+
+
+
+
+
+
+

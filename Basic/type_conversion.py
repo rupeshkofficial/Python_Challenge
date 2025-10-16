476d414a65f17f87be1c8363f74f431a773390ee
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-16 14:13:11 +0530

    Day_1

---
+++ Basic/type_conversion.py
@@ -0,0 +1,19 @@
+# Converting one data type to another data type
+
+x = "100"
+y = "200"
+
+a = int(x)
+b = int(y)
+
+z = a + b
+print(z)
+
+p = "100.3"
+q = "200"
+
+r = float(p)
+s = float(q)
+
+t = r + s
+print(t)

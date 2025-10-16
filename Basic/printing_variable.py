83e0d0f5e3a2cf8a0daaeb06300ba4df9da2c791
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-16 12:34:36 +0530

    Day_1

--- Basic/printing_variable.py
+++ Basic/printing_variable.py
@@ -3,6 +3,21 @@ age = 22
 gender = "Male"
 
 # Method 1
-print(name)
-print(age)
-print(gender)
+print("My name is",name)
+print("My age is",age)
+print("My gender is",gender)
+
+print("My name is",name,"My age is",age,"My gender is",gender)
+
+# Method 2 (F-Strings)
+
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"my gender is {gender}")
+print(f"my name is {name} my age is {age} and gender is {gender}")
+
+
+
+
+
+

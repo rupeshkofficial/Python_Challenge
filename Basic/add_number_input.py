c89db7789aca8addab0dad02c52778338de138c6
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-16 14:41:33 +0530

    Day_1

---
+++ Basic/add_number_input.py
@@ -0,0 +1,8 @@
+x = int(input("Enter Your First Number:"))
+y = int(input("Enter Your Second Number:"))
+z = int(input("Enter Your Third Number:"))
+p = int(input("Enter Your Fourth Number:"))
+
+s = x + y + z + p
+
+print(f"Addition of {x},{y},{z} and {p} is: {s}")
---
+++ Basic/input_fn.py
@@ -0,0 +1,18 @@
+# name = "Rupesh"
+# age = 22
+
+# print(f"My name is {name}")
+# print(f"my age is {age}")
+
+
+# x = input()
+# print("You Entered")
+# print(x)
+
+
+name = input("Enter Your Name:")
+age = input("Enter Your age:")
+gender = input("Enter you gender:")
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My gender is {gender}")

1914a411f0ab4184ef9785e03a052a10f11eb2d4
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-17 11:36:46 +0530

    Day_2

---
+++ Conditional_statments/ques_1.py
@@ -0,0 +1,13 @@
+'''
+Ask 2 numbers from user
+print which number is the greatest.
+'''
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if ( a > b):
+    print(f"First number {a} is greater.")
+else:
+    print(f"Second number {b} is greater.")
---
+++ Conditional_statments/ques_2.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter 1 number: "))
+
+if(num1 % 2 == 0):
+    print(f"{num1} is even")
+else:
+    print(f"{num1} is odd")

c65f40a129a49aa96a0415f315200bdeb4528208
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-17 15:32:26 +0530

    Day_2

--- Conditional_statments/if_elif_else.py
+++ Conditional_statments/if_elif_else.py
@@ -17,3 +17,15 @@ else:
     -----
 
 '''
+
+# Code 1
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+if num1 > num2:
+    print("Number 1 is greater")
+elif num2 > num1:
+    print("Number 2 is greater")
+else:
+    print("Both are equal")

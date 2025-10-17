d9d79decb6cb569bf9c1b97fecf2038eab186821
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-17 10:38:07 +0530

    Day_2

---
+++ Operators/assigment_operators.py
@@ -0,0 +1,19 @@
+# Assignment Operator (=) are used to assign values to variables.
+
+x = 10 # 10 assign to x
+x = 20 # Now, x override by 20 and it's will print 20
+print(x)
+
+
+x = 10
+#x = x +5
+x+=5
+print(x)
+
+
+x = 10
+x*=4  # x = x*4
+print(x)
+
+x **=3 # x = x **3
+print(x) 

bdaa13a82750f0fb13445a9aca425ae31e09d209
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-17 10:05:26 +0530

    Day_2

--- Operators/arithmetic_operators.py
+++ Operators/arithmetic_operators.py
@@ -12,6 +12,24 @@ Different Types of Operators:-
 6. Membership Operators 
 7. Bitwise Operators
 
+Note:
+
+1. Modulus ( % ) --> Gives remainder --> Ex: 10 % 3 = 1 , 5% 3 = 2, 
+                                         5 % 10 =  5 ( Jab left wali value choti hoti hai right wali value se to remainder left wali value hoti hai)
+                                         9 % 10 = 9
+                                         10 % 10 = 0
+
+2. Exponential ( ** ) --> Power --> Ex: 5**3 = 125
+
+3. Division ( % ) --> Output always in float ---> Ex: 10 % 4 = 2.5, 10 % 10 = 1.0
+
+4. Floor Division ( // ) --> Output always in integer ( That is always gives lower integer) 
+                       
+                         Ex: 10 % 4 = 2.5 but we do 10 // 4 (floor division) then we have to take lower integer then it's output will be 2.
+                            13 % 7 = 1.8571 but we do 13 // 7 (floor division) then we have to take lower integer then it's output will be 1.
+                             
+                                        
+
 ''' 
 
 a = 40

9e92df8f5cfce49937c89f9ff6aecc35f0434853
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-17 09:52:49 +0530

    Day_2

---
+++ Operators/arithmetic_operators.py
@@ -0,0 +1,28 @@
+# Operators ---> To perform operations on varibales and values
+
+'''
+Different Types of Operators:-
+
+1. Arithmetic Operators --> To perform mathematical operations ( +, -, %, /, **, //)
+2. Assignment Operators  ---> =
+3. Comparison Operators 
+4. Logical Operators 
+
+5. Identity Operators 
+6. Membership Operators 
+7. Bitwise Operators
+
+''' 
+
+a = 40
+b = 4
+
+print(a+b)
+print(a-b)
+print(a/b) 
+print(a*b)
+
+print(a**b)
+print(a%b) # % ---> Gives remainder
+
+print(a//b) # // --> Floor divison (Return always integer)

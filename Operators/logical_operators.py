dae6a670dbb216161f45890bfbebbbee1baea37b
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-17 10:53:23 +0530

    Day_2

---
+++ Operators/logical_operators.py
@@ -0,0 +1,8 @@
+'''
+Logical Operators are used to combine conditional statments.
+
+AND ---> Returns TRUE if both the statments are true.               Ex: x < 10 and y < 15
+OR  ---> Returns TRUE if one of the statments is true.              Ex: x < 15 or x < 24
+NOT ---> Reverse the results, Returns FALSE if the result is true.  Ex: not(x < 15 and x < 120)
+
+'''

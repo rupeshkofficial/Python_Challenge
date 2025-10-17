df684a978473c1dbf7911b48f80867da3f7b7477
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-17 10:46:36 +0530

    Day_2

---
+++ Operators/comparison_operators.py
@@ -0,0 +1,15 @@
+'''
+===> Comaprison Operators ===> Used to compare two values and it's answer will always be TRUE or FALSE ( BOOLEAN VALUE)
+
+'''
+
+a = 20
+b = 21
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+

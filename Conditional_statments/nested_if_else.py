57e98e5a15f6b35994c10de79ce2e41575d3bf91
rupeshkofficial <officialrupeshk@gmail.com>
2025-10-24 10:51:50 +0530

    Day_4

--- Conditional_statments/nested_if_else.py
+++ Conditional_statments/nested_if_else.py
@@ -12,10 +12,19 @@ elif
 
 num = int(input("Enter a number: "))
 
-if num >= 0:
-    if num > 0:
-        print("Positive")
-    else:
-        print("Equal to Zero")
+# if num >= 0:
+#     if num > 0:
+#         print("Positive")
+#     else:
+#         print("Equal to Zero")
+# else:
+#     print("negative")
+
+
+if num > 0:
+    print("Positive")
 else:
-    print("negative")
+    if num == 0:
+        print("Equal to Zero")
+    else:
+        print("Negative")

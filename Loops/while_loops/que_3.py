60ffdd58138dadf9270cd605685bc8a5733ceb61
rupeshkofficial <officialrupeshk@gmail.com>
2025-11-14 15:49:14 +0530

    Day_5

---
+++ Loops/while_loops/que_3.py
@@ -0,0 +1,18 @@
+# Print Even Numbers between 1 to 20
+
+''' 
+
+i = 2
+while i <= 20:
+    print(i, end = " ")
+    i = i + 2
+
+'''
+
+i = 1
+
+while i <= 20:
+    if i % 2 == 0:
+        print(i, end = " ")
+    i = i + 1
+
--- Loops/while_loops/ques_2.py
+++ Loops/while_loops/ques_2.py
@@ -14,5 +14,6 @@ while i < 11:
 i = 1 
 
 while i <=10:
-    print(f"Number:", i)
+    # print(f"Number:", i)
+    print(i,end= " ") # To print no in same line like this 1 2 3 4 5 6 7 8 9 10
     i = i + 1

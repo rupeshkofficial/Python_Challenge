ac0c1a6a5bd98e83bbcadb1a339d364c59bad0b7
rupeshkofficial <officialrupeshk@gmail.com>
2025-11-17 14:33:23 +0530

    Day_6

---
+++ Loops/control_statments/break_statments.py
@@ -0,0 +1,13 @@
+'''
+
+for i in range(1,101):
+    print(i, end = " ") # 1 2 3 4 5 6 7 8 9 10 
+    if i == 10:
+        break
+
+'''
+
+for i in range(1,11):
+    if i == 5:
+        break
+    print(i, end = " ") # 1 2 3 4 

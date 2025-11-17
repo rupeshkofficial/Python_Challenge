ab33715d2734181d01daaa94f41e9d63a2c61691
rupeshkofficial <officialrupeshk@gmail.com>
2025-11-17 12:13:10 +0530

    Day_6

--- Loops/for_loops/ques_1.py
+++ Loops/for_loops/ques_1.py
@@ -1,4 +1,14 @@
 # print 1 to 10
+# Here: 1 including and 10 excluding
 
-for i in range(1,10):
+'''
+
+for i in range(1,11):
+    print(i, end = " ")
+
+'''
+
+for i in range(1,11):
+    print("Done")
     print(i)
+    print("BYE")

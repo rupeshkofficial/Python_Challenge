c2877f1c0d40adf16330e73ad1fee11c11bbe27e
rupeshkofficial <officialrupeshk@gmail.com>
2025-11-17 16:32:33 +0530

    Day_6

---
+++ List/type_to_store.py
@@ -0,0 +1,3 @@
+x = [1,2,3,4,44.5,56.8,"Rupesh",False,True,"Rupesh Tech"]
+print(x)
+print(type(x))
---
+++ List/what_are_lists.py
@@ -0,0 +1,9 @@
+'''
+
+Lists --> To store multiple values in a single variables. || Multiple data types can store in lists
+
+'''
+
+rupesh = [34,45,6,7,8,3,6]
+print(rupesh)
+print(type(rupesh))

c81cb728003649bede07abeb0d7ffd369c8ae709
rupeshkofficial <officialrupeshk@gmail.com>
2025-11-20 15:12:14 +0530

    Day_6

--- Loops/nested_loops/advance_level_nested_loops_questions/ques_4.py
+++ Loops/nested_loops/advance_level_nested_loops_questions/ques_4.py
@@ -4,3 +4,10 @@ for i in range(1, 6):
     for k in range((i * 2) - 1):
         print("*", end=" ")
     print()
+
+for i in range(4,0,-1):
+    for j in range(5,i,-1):
+        print(" ",end=" ")
+    for k in range((i*2)-1):
+        print("*",end=" ")
+    print()

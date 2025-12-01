6817de99a0ea0775ba734c5c6aa15ccfdba29512
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-01 13:26:29 +0530

    Day_7

---
+++ Introduction to LIsts/List Function and Methods Questions/ques_1.py
@@ -0,0 +1,15 @@
+"""
+Write a program that prompts the user to specify the length of a list and then request numbers to populate
+that list. Display the final list as the output.
+
+"""
+
+list_length = int(input("Enter the length of the list: "))
+result = []
+for i in range(0,list_length):
+    #num = int(input("Enter number: "))
+    num = int(input(f"Enter number at position {i}: "))
+    result.append(num)
+print(result)
+
+

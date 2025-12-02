e9ffb8daeade564bc0f688400e3ac86d2205d434
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-02 13:25:36 +0530

    Day_8

---
+++ Introduction to Strings/intro_to_string.py
@@ -0,0 +1,14 @@
+# String ---> Iterable, Immutable
+
+a = "Rupesh Kumar"
+#print(a)
+#print(type(a))
+
+#a[0] = "z"
+#print(a) # TypeError: 'str' object does not support item assignment As it is immutable
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[-2])
+print(a[-4])
---
+++ Introduction to Strings/iterate_strings.py
@@ -0,0 +1,14 @@
+my_string = "Rupesh Tech"
+
+# By Index 
+for index in range(0,len(my_string)):
+    print(my_string[index])
+
+
+# By Value
+for ch in my_string:
+    print(ch)
+
+# Reverse String
+for i in range(len(my_string)-1,-1,-1):
+    print(my_string[i])

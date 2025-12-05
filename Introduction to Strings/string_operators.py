df554ae103ffbd83dece3e606c5284cd073a6515
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-05 14:47:22 +0530

    Day_9

--- Introduction to Strings/string_operators.py
+++ Introduction to Strings/string_operators.py
@@ -10,3 +10,17 @@ first_char = "a"  # 97
 second_char = "A"  # 65
 print(first_char > second_char)  # True
 print(first_char < second_char)  # False
+
+
+first_char = "anirudh"  
+second_char = "anirudH"  
+print(first_char > second_char)  # True
+print(first_char < second_char) # False
+
+# To print ASCII Code
+ch = "A"
+print(ord(ch)) # 65
+
+
+num = 97
+print(chr(num)) # a

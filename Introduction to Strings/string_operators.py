3153b95cf64dc0231c642cf0a74ebfdf560346c6
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-05 14:43:25 +0530

    Day_9

---
+++ Introduction to Strings/string_operators.py
@@ -0,0 +1,12 @@
+a = "Rupesh"
+b = "Tech"
+print(a + b)  # RupeshTech
+print(a * 3)  # RupeshRupeshRupesh
+print(a == b)  # False
+
+
+# ASCII Codes
+first_char = "a"  # 97
+second_char = "A"  # 65
+print(first_char > second_char)  # True
+print(first_char < second_char)  # False

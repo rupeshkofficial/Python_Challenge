aff9d8d570519cee3cf3887222fd1fe88397055d
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-09 14:38:32 +0530

    Day_10

--- Introduction to Strings/String_Method Part 2 Questions (Split and Join)/ques_4.py
+++ Introduction to Strings/String_Method Part 2 Questions (Split and Join)/ques_4.py
@@ -4,3 +4,15 @@ Write a program that converts a string in Camel_Case to Snake_Case.
 Ex: "helloWorldHowAreYou" ----> O/P: "hello_world_how_are_you"
 """
 
+my_string = "helloWorldHowAreYou"
+result = ""
+
+for ch in my_string:
+    if ch.isupper():
+        result += "_" + ch.lower()
+    else:
+        result += ch
+
+print(result)
+
+

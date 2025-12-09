7bdca10a3f90045f07df4b7eed2b97dbf0f472c0
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-09 16:32:45 +0530

    Day_11

--- 8. Introduction to Dictionary/add_update.py
+++ 8. Introduction to Dictionary/add_update.py
@@ -1,3 +1,14 @@
 my_dict = {"name": "Rupesh","age": 23, "gender": "Male"}
 
-print(my_dict)
+print(my_dict)    # {'name': 'Rupesh', 'age': 23, 'gender': 'Male'}
+
+
+# ------- Method 1 ( If key exist then update if key not exist then it will add)
+
+# Example ( Key Exist --> Update)
+my_dict["age"] = 100 
+print(my_dict)   # {'name': 'Rupesh', 'age': 100, 'gender': 'Male'}
+
+# Example ( Key not Exist --> Added)
+my_dict["xyz"] = 234
+print(my_dict) # {'name': 'Rupesh', 'age': 100, 'gender': 'Male', 'xyz': 234}

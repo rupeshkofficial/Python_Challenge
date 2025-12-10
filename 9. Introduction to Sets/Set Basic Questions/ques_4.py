e14ff2f97df7f13c7e5ddf44903de8472e071efa
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-10 15:46:19 +0530

    Day_12

--- 9. Introduction to Sets/Set Basic Questions/ques_4.py
+++ 9. Introduction to Sets/Set Basic Questions/ques_4.py
@@ -1,3 +1,8 @@
 """
 Write a python program to remove all elements from a given set.
 """
+
+my_set = {5,6,2,1,"Rupesh",7}
+
+my_set.clear()
+print(my_set) # set()
--- 9. Introduction to Sets/Set Basic Questions/ques_5.py
+++ 9. Introduction to Sets/Set Basic Questions/ques_5.py
@@ -1,3 +1,11 @@
 """
 Write a python program to find the length of a set.
 """
+my_set = {5,6,2,1,"Rupesh",7}
+
+#print(len(my_set)) # 6
+
+count = 0
+for i in my_set:
+    count += 1
+print(count) # 6

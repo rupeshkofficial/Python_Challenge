76b38dc51c734100848a091bef9b74873af5800f
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-11 09:53:43 +0530

    Day_13

---
+++ Functions/intro.py
@@ -0,0 +1,19 @@
+'''
+Functions ---> Block of code
+Always put fn at top of the code
+
+start --> def greet():
+              ------
+              ------
+              ------
+fn call-> greet()
+
+
+'''
+
+def greet():
+    print("Hello World!")
+    print("Good Bye!")
+
+print("Hello Rupesh")
+greet()

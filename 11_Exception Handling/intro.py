d237d9ba330193b47a6ee8fe5abf5f4865a004fa
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-11 11:58:09 +0530

    Day_13

---
+++ 11_Exception Handling/intro.py
@@ -0,0 +1,13 @@
+# What is Exception Handling?
+
+try:
+    lst = [3,4,5,7,98,6,89,5]
+    print(lst[1])
+    print(lst[2])
+    print(lst[65])
+    print(lst[3])
+    print(lst[7])
+except:
+    print("Some error occured")
+print("Done")
+print("Bye")

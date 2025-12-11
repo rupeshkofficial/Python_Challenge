886dffee7f14081d9e13a1ff78a4e9e302e20f2d
rupeshkofficial <officialrupeshk@gmail.com>
2025-12-11 13:37:40 +0530

    Day_14

---
+++ 12_File Handling/Append_mode.py/intro.py
@@ -0,0 +1,6 @@
+# Append will not overide the file buit it will add new tetx in that file without effecting previous written text.
+
+with open ("/Users/rupeshk/Desktop/Coding/Python_Challenge/12_File Handling/Append_mode.py/hello.txt","a") as f:
+    f.write("\nA new line")
+
+
